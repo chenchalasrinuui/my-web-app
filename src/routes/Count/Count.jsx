2b81ea4792ed8db69b50ef8fa17f0e2de4f54b3c
chenchalasrinuui <66296488+chenchalasrinuui@users.noreply.github.com>
2024-07-23 10:50:03 +0530

    added tests

--- src/routes/Count/Count.jsx
+++ src/routes/Count/Count.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
+"use client"
+import React, { Fragment, useState } from 'react'
 import styles from './Count.module.css'
 export const Count = () => {
-    return (
-        <div className={styles.clr}>Count</div>
+    const [count, setConnt] = useState(0)
+    const fnIncrement = () => {
+        setConnt(count + 1);
+    }
+    return (<Fragment >
+        <div data-testid="count" className={styles.clr}>Count</div>
+        <h1>{count}</h1>
+        <button onClick={fnIncrement}>Increment</button>
+    </Fragment>
     )
 }
